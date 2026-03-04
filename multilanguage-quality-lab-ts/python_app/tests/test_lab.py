d434d1cd343c2cd601ff26bc78540f641d950819
JuanmaZapataOlaya <124525725+JuanmaZapataOlaya@users.noreply.github.com>
2026-03-04 15:08:10 +0000

    Pruebas PalindromoPY

--- multilanguage-quality-lab-ts/python_app/tests/test_lab.py
+++ multilanguage-quality-lab-ts/python_app/tests/test_lab.py
@@ -1,11 +1,14 @@
+from python_app.palindromo import es_palindromo
+#from utils import suma
 
-# TODO: Escribe pruebas unitarias para es_palindromo y suma.
-# Sugerencias:
-# - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
-# - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
+def test_es_palindromo():
+    assert es_palindromo("Radar") == True
 
-from palindromo import es_palindromo
-from utils import suma
+def test_es_palindromo2():
+    assert es_palindromo("anita lava la tina") == True
 
-def test_ejemplo_siembra():
-    assert True
+def test_es_palindromo3():
+    assert es_palindromo("") == True
+
+def test_es_palindromo4():
+    assert es_palindromo("Python") == False
